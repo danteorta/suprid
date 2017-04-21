05ec71a0611a869d2ec96ccf19c453ae8b60cd23
Dante <danteorta@gmail.com>
2017-04-20 21:17:19 -0700

    New code for visualization of the wellbore ids

--- data_exploration.py
+++ data_exploration.py
@@ -84,6 +84,86 @@ pl.show(False)
 
 well = pn.read_csv(wells_path, delim_whitespace=True, skiprows=1, index_col=False)
 
+
+
+
+# ----------------- Read vtk fancy way -----------------
+from vtk import *
+from vtk.util.numpy_support import vtk_to_numpy
+import pandas as pn
+
+vtk_file = 'D:\\Users\\dorta\Dropbox\Stanford\Research\workspace\\test_run\DiscretizationToolkit\\output_mesh.vtk'
+
+# load a vtk file as input
+reader = vtk.vtkUnstructuredGridReader()
+reader.SetFileName(vtk_file)
+reader.ReadAllVectorsOn()
+reader.ReadAllScalarsOn()
+reader.Update()
+data = reader.GetOutput()
+
+# Map of cell_id to flowcell_id
+flowcell_ids = pn.DataFrame(vtk_to_numpy(data.GetCellData().GetArray('FLOWCELL_ID')), columns=['flowcell_id'])
+flowcell_ids['cell_id'] = flowcell_ids.index
+# Coordinates of each node
+pts_coords = pn.DataFrame(vtk_to_numpy(data.GetPoints().GetData()), columns=['x', 'y', 'z'])
+pts_coords['node_id'] = pts_coords.index
+
+# cell_locations = index where the integer with the number of nodes in each cell is located
+# cell_data[cell_locations] <-- Number of nodes in each cell
+cell_locations = vtk_to_numpy(data.GetCellLocationsArray())
+cell_data = vtk_to_numpy(data.GetCells().GetData())
+
+nodes_dict = []
+for c in range(len(cell_locations)):
+    stt = cell_locations[c] + 1
+    # last case
+    if c == len(cell_locations)-1:
+        pt_ids = cell_data[stt:]
+    else:
+        end = cell_locations[c+1]
+        pt_ids = cell_data[stt:end]
+    nodes_dict.append(pt_ids)
+
+# Dataframe with the ids of the nodes corresponding to each cell
+all_nodes = pn.DataFrame(nodes_dict).stack().reset_index()
+all_nodes.rename(columns={'level_0': 'cell_id', 'level_1': 'cell_node_count', 0: 'node_id'}, inplace=True)
+all_nodes.loc[:, ['cell_id', 'node_id']] = all_nodes.loc[:, ['cell_id', 'node_id']].astype(int)
+
+all_nodes = all_nodes.merge(pts_coords, on='node_id', how='left')
+
+# Get centroids
+centroids = all_nodes.loc[:,['cell_id','x','y','z']].groupby('cell_id').agg('mean').reset_index()
+centroids = centroids.merge(flowcell_ids, on='cell_id', how='left')
+
+# These are the correct ones
+wb_ids = wellbore_data.IB.unique()
+
+centroids.loc[:,'wellbore'] = 0
+centroids.loc[centroids.flowcell_id.isin(wb_ids), 'wellbore'] = 1
+
+
+ax = centroids.loc[centroids.wellbore==1].plot.scatter(x='x', y='y', color='red', label='wellbore');
+
+centroids.loc[centroids.wellbore==0].plot.scatter(x='x', y='y', color='gray', label='other', ax=ax);
+
+
+wb = wellbore_data.loc[:, ['T', 'IB', 'time']].merge(centroids, how='left', left_on='IB', right_on='flowcell_id')
+wb.loc[:, 'T'] = wellbore_data.loc[:, 'T'] - 273
+temp_series = wb.pivot(values='T', index='x', columns='time').sort_index()
+pl.pcolor(temp_series.values)
+pl.colorbar()
+pl.show(False)
+
+
+a = wb.drop_duplicates(subset=['flowcell_id'])
+a.plot.scatter(x='x', y='y')
+
+# Read scalar names
+for m in range(reader.GetNumberOfScalarsInFile()):
+    print(reader.GetScalarsNameInFile(m))
+
+
 # #  -------------------- Read vtk file brute force --------------------
 # vtk_file = 'D:\\Users\\dorta\Dropbox\Stanford\Research\workspace\\test_run\DiscretizationToolkit\\output_mesh.vtk'
 # node_coords = pn.read_csv(vtk_file, delim_whitespace=True, skiprows=5, nrows=16650, index_col=False,
@@ -109,55 +189,3 @@ well = pn.read_csv(wells_path, delim_whitespace=True, skiprows=1, index_col=Fals
 # nodes = pn.DataFrame.from_dict(data_str,orient='index')
 #
 #
-
-
-# ----------------- Read vtk fancy way -----------------
-from vtk import *
-from vtk.util.numpy_support import vtk_to_numpy
-import pandas as pn
-
-vtk_file = 'D:\\Users\\dorta\Dropbox\Stanford\Research\workspace\\test_run\DiscretizationToolkit\\output_mesh.vtk'
-
-# load a vtk file as input
-reader = vtk.vtkUnstructuredGridReader()
-reader.SetFileName(vtk_file)
-reader.ReadAllVectorsOn()
-reader.ReadAllScalarsOn()
-reader.Update()
-data = reader.GetOutput()
-
-# Map of cell_id to flowcell_id
-flowcell_ids = pn.DataFrame(vtk_to_numpy(data.GetCellData().GetArray('FLOWCELL_ID')), columns=['flowcell_id'])
-# Coordinates of each node
-pts_coords = pn.DataFrame(vtk_to_numpy(data.GetPoints().GetData()), columns=['x', 'y', 'z'])
-pts_coords['node_id'] = pts_coords.index
-
-cell_locations = vtk_to_numpy(data.GetCellLocationsArray())
-cell_data = vtk_to_numpy(data.GetCells().GetData())
-
-nodes_dict = []
-for c in range(len(cell_locations)):
-    stt = cell_locations[c] + 1
-    # last case
-    if c == len(cell_locations)-1:
-        pt_ids = cell_data[stt:]
-    else:
-        end = cell_locations[c+1]
-        pt_ids = cell_data[stt:end]
-    nodes_dict.append(pt_ids)
-
-# Dataframe with the ids of the nodes corresponding to each cell
-all_nodes = pn.DataFrame(nodes_dict).stack().reset_index()
-all_nodes.rename(columns={'level_0': 'cell_id', 'level_1': 'cell_node_count', 0: 'node_id'}, inplace=True)
-all_nodes.loc[:, ['cell_id', 'node_id']] = all_nodes.loc[:, ['cell_id', 'node_id']].astype(int)
-
-all_nodes = all_nodes.merge(pts_coords, on='node_id', how='left')
-
-# Get centroids
-centroids = all_nodes.loc[:,['cell_id','x','y','z']].groupby('cell_id').agg('mean').reset_index()
-
-# Read scalar names
-for m in range(reader.GetNumberOfScalarsInFile()):
-    print(reader.GetScalarsNameInFile(m))
-
-
