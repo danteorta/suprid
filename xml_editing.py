701d13f406cd6086f595d3039017509e61f8ac7a
Dante <danteorta@gmail.com>
2017-04-24 12:52:04 -0700

    xml parsing without loosing cdata tag

--- xml_editing.py
+++ xml_editing.py
@@ -1,13 +1,22 @@
-import xml.etree.ElementTree as etree
+# import xml.etree.ElementTree as etree
 import pandas as pn
 from io import StringIO
+from lxml.etree import CDATA
+from lxml import etree
 import numpy as np
+
+
+
 
 # Open the XML file
 files_path = 'D:\\Users\dorta\Dropbox\Stanford\Research\work' \
             'space\\test_run\DiscretizationToolkit\\'
 orig_filename = 'xDante.xml'
-tree = etree.parse(files_path + orig_filename)
+
+
+parser = etree.XMLParser(strip_cdata=False)
+# root = etree.XML(files_path + orig_filename, parser)
+tree = etree.parse(files_path + orig_filename, parser)
 root = tree.getroot()
 
 # Read the relevant features
@@ -28,7 +37,8 @@ cond_data = cond_data.astype({'id_face':str, 'cond': str})
 b = cond_data.values.tolist()
 step_1 = ['\t\t\t\t'.join(elm) for elm in b]
 step_2 = '\n\t\t\t\n\t\t\t' + '\n\t\t\t'.join(step_1) + '\n\t\t\t\n\t\t\t'
-
-conductivity.text = step_2
+step_3 = CDATA(step_2)
+# step_3 = "\n<{0}{1}]]>\n".format('![CDATA[', step_2)
+conductivity.text = step_3
 tree.write(files_path + 'test_xml.xml')
 
